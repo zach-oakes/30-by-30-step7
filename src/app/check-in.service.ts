8cd01b04d110a12848b70ddd0b1a7a8c952a99e0
Zach Oakes <zoakes1@gmail.com>
2024-02-05 14:23:32 -0700

    Update to service layer

--- src/app/check-in.service.ts
+++ src/app/check-in.service.ts
@@ -2,22 +2,63 @@ import { Injectable } from '@angular/core';
 import {CheckIn} from "./check-in";
 import {HttpClient} from "@angular/common/http";
 import {catchError, Observable, of} from "rxjs";
+import {v4} from "uuid";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckInService {
+  checkIns: CheckIn[] = [];
+  selectedCheckIn: CheckIn = {} as CheckIn;
 
   private url = 'https://mock-json-server-five.vercel.app/checkIns';
 
   constructor(private http: HttpClient) {}
 
-  getCheckIns(): Observable<CheckIn[]> {
-    return this.http.get<CheckIn[]>(this.url)
+  getCheckIns() {
+    this.http.get<CheckIn[]>(this.url)
         .pipe(
             // return an empty array here if there was an error on the request
             catchError(this.handleError<CheckIn[]>([]))
-        );
+        ).subscribe(resp => this.checkIns = resp);
+  }
+
+  deleteCheckIn(checkIn: CheckIn): void {
+    this.checkIns = this.checkIns.filter(c => c.id !== checkIn.id);
+  }
+
+  selectCheckIn(checkIn: CheckIn): void {
+    // Select
+    if (!this.selectedCheckIn || this.selectedCheckIn.id !== checkIn.id) {
+      this.selectedCheckIn = {...checkIn};
+    } else {
+      // Unselect
+      this.selectedCheckIn = {} as CheckIn;
+    }
+  }
+
+  removeAllCheckIns(): void {
+    this.checkIns = [];
+    this.selectedCheckIn = {} as CheckIn;
+  }
+
+  editCheckIn(checkIn: CheckIn): void {
+    const index = this.checkIns.findIndex(c => c.id === checkIn.id);
+
+    // -1 is not found
+    if (index !== -1) {
+      this.checkIns[index] = checkIn;
+    }
+
+    this.selectedCheckIn = {} as CheckIn;
+  }
+
+  addCheckIn(checkIn: CheckIn): void {
+    const toAdd: CheckIn = {...checkIn};
+    toAdd.id = v4();
+
+    this.checkIns.push(toAdd);
+    this.selectedCheckIn = {} as CheckIn;
   }
 
   private handleError<T>(result?: T) {
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,54 +11,14 @@ import {ServingStyle} from "../serving-style";
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-  checkIns: CheckIn[] = [];
   beerTypes: string[] = Object.values(BeerType);
   servingStyles: string[] = Object.values(ServingStyle);
   ratings: number[] = [1, 2, 3, 4, 5];
-  selectedCheckIn: CheckIn = {} as CheckIn;
 
-  constructor(private checkInService: CheckInService) {}
+  constructor(public checkInService: CheckInService) {}
 
-  ngOnInit(): void {
-    this.checkInService.getCheckIns()
-        .subscribe(result => this.checkIns = result);
+  ngOnInit() {
+    this.checkInService.getCheckIns();
   }
 
-  deleteCheckIn(checkIn: CheckIn): void {
-    this.checkIns = this.checkIns.filter(c => c.id !== checkIn.id);
-  }
-
-  selectCheckIn(checkIn: CheckIn): void {
-    // Select
-    if (!this.selectedCheckIn || this.selectedCheckIn.id !== checkIn.id) {
-      this.selectedCheckIn = {...checkIn};
-    } else {
-      // Unselect
-      this.selectedCheckIn = {} as CheckIn;
-    }
-  }
-
-  removeAllCheckIns(): void {
-    this.checkIns = [];
-    this.selectedCheckIn = {} as CheckIn;
-  }
-
-  editCheckIn(checkIn: CheckIn): void {
-    const index = this.checkIns.findIndex(c => c.id === checkIn.id);
-
-    // -1 is not found
-    if (index !== -1) {
-      this.checkIns[index] = checkIn;
-    }
-
-    this.selectedCheckIn = {} as CheckIn;
-  }
-
-  addCheckIn(checkIn: CheckIn): void {
-    const toAdd: CheckIn = {...checkIn};
-    toAdd.id = v4();
-
-    this.checkIns.push(toAdd);
-    this.selectedCheckIn = {} as CheckIn;
-  }
 }
